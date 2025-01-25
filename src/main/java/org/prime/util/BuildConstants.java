1827a3f068cafb8ce5a7e218b717fd581a84750c
James <jamesbhaney53@gmail.com>
2025-01-25 15:27:52 -0600

    Enabled Optimize

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 31;
-  public static final String GIT_SHA = "828fea75f7b0e46fc46ff31167f7681fbe795cb4";
-  public static final String GIT_DATE = "2025-01-25 14:57:42 CST";
-  public static final String GIT_BRANCH = "HEAD";
-  public static final String BUILD_DATE = "2025-01-25 15:08:23 CST";
-  public static final long BUILD_UNIX_TIME = 1737839303745L;
+  public static final int GIT_REVISION = 32;
+  public static final String GIT_SHA = "a9dec67aea32f215275826868d6c9d6cc21851d0";
+  public static final String GIT_DATE = "2025-01-25 15:13:13 CST";
+  public static final String GIT_BRANCH = "drivetraintesting2";
+  public static final String BUILD_DATE = "2025-01-25 15:24:51 CST";
+  public static final long BUILD_UNIX_TIME = 1737840291822L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
