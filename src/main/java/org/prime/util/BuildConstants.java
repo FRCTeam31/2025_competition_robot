ff9759597cd0e883c508a12688895c1b9f2cf4f7
James <jamesbhaney53@gmail.com>
2025-01-27 18:09:43 -0600

    library updtates

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 36;
-  public static final String GIT_SHA = "91d8e8e13e7e91fcb2c27e1e1e3bf6efa849144d";
-  public static final String GIT_DATE = "2025-01-25 17:44:37 CST";
+  public static final int GIT_REVISION = 38;
+  public static final String GIT_SHA = "c31e28309ee9308f00d0bbe082567f7f8497109c";
+  public static final String GIT_DATE = "2025-01-27 18:04:15 CST";
   public static final String GIT_BRANCH = "drivetraintesting2";
-  public static final String BUILD_DATE = "2025-01-27 17:31:31 CST";
-  public static final long BUILD_UNIX_TIME = 1738020691785L;
+  public static final String BUILD_DATE = "2025-01-27 18:09:16 CST";
+  public static final long BUILD_UNIX_TIME = 1738022956298L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
