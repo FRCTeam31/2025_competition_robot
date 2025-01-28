0a2eff96a136343285bf6eccbe7860a0981a054f
James <jamesbhaney53@gmail.com>
2025-01-27 19:08:32 -0600

    path planner changes

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 38;
-  public static final String GIT_SHA = "c31e28309ee9308f00d0bbe082567f7f8497109c";
-  public static final String GIT_DATE = "2025-01-27 18:04:15 CST";
+  public static final int GIT_REVISION = 40;
+  public static final String GIT_SHA = "03b89aac6da15de286b0301c2fea1b8b4b4716c2";
+  public static final String GIT_DATE = "2025-01-27 18:30:01 CST";
   public static final String GIT_BRANCH = "drivetraintesting2";
-  public static final String BUILD_DATE = "2025-01-27 18:09:16 CST";
-  public static final long BUILD_UNIX_TIME = 1738022956298L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-27 18:55:53 CST";
+  public static final long BUILD_UNIX_TIME = 1738025753596L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
