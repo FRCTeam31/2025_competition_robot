a3431a1153141573a2a3c604ccb0a20a355d8a23
James <jamesbhaney53@gmail.com>
2025-02-06 16:46:09 -0600

    Removed jvm arguments

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 48;
-  public static final String GIT_SHA = "0b592b7a0a1c24248388263ba7389fdf879362be";
-  public static final String GIT_DATE = "2025-02-02 11:51:02 CST";
+  public static final int GIT_REVISION = 51;
+  public static final String GIT_SHA = "804f44856ed8a3846a5d434b75822aad70b0e29e";
+  public static final String GIT_DATE = "2025-02-03 13:18:52 CST";
   public static final String GIT_BRANCH = "adk-testing";
-  public static final String BUILD_DATE = "2025-02-02 12:14:24 CST";
-  public static final long BUILD_UNIX_TIME = 1738520064009L;
+  public static final String BUILD_DATE = "2025-02-06 16:44:26 CST";
+  public static final long BUILD_UNIX_TIME = 1738881866823L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
