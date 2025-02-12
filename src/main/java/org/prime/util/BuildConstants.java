ec4f0becccdd6377d4387c6b5c512cadcf5cc566
ArtsNCrafters <howtoliveinpain@gmail.com>
2025-02-11 18:37:16 -0600

    Built Code

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 59;
-  public static final String GIT_SHA = "85e2e84c1c4fe0f51362d136c56b9f0916c369f6";
-  public static final String GIT_DATE = "2025-02-07 12:28:47 CST";
-  public static final String GIT_BRANCH = "adk-testing";
-  public static final String BUILD_DATE = "2025-02-07 13:05:35 CST";
-  public static final long BUILD_UNIX_TIME = 1738955135650L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 21;
+  public static final String GIT_SHA = "82354fb689597499a797205dfec074dc789e7d76";
+  public static final String GIT_DATE = "2025-02-11 18:32:47 CST";
+  public static final String GIT_BRANCH = "pathplanner_fixes2";
+  public static final String BUILD_DATE = "2025-02-11 18:36:37 CST";
+  public static final long BUILD_UNIX_TIME = 1739320597706L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
