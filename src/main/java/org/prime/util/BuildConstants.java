7b976c46e44df19ee4d74bda7cb5cd49bfaf2ba6
ArrowVark <arrowvark.gaming@gmail.com>
2025-02-15 11:49:33 -0600

    current version of robot code

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 36;
-  public static final String GIT_SHA = "08b2a8b2d500853b2eb5f667de5dbec5d8903457";
-  public static final String GIT_DATE = "2025-02-13 18:12:33 CST";
+  public static final int GIT_REVISION = 37;
+  public static final String GIT_SHA = "777f6ad9641ee31125b932149aa8d9bb966df29a";
+  public static final String GIT_DATE = "2025-02-13 19:12:25 CST";
   public static final String GIT_BRANCH = "pathplanner_fixes2";
-  public static final String BUILD_DATE = "2025-02-13 19:09:40 CST";
-  public static final long BUILD_UNIX_TIME = 1739495380925L;
+  public static final String BUILD_DATE = "2025-02-15 11:43:12 CST";
+  public static final long BUILD_UNIX_TIME = 1739641392889L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
