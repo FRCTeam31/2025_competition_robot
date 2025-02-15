e9a9392e647de60848c3986bd170effe9bbd72de
James <jamesbhaney53@gmail.com>
2025-02-15 10:29:18 -0600

    attempted to implement progfiling with visual vm

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 36;
-  public static final String GIT_SHA = "08b2a8b2d500853b2eb5f667de5dbec5d8903457";
-  public static final String GIT_DATE = "2025-02-13 18:12:33 CST";
+  public static final int GIT_REVISION = 39;
+  public static final String GIT_SHA = "45afae9f450a91641260a8ea88931a6ba667a2c9";
+  public static final String GIT_DATE = "2025-02-14 17:01:56 CST";
   public static final String GIT_BRANCH = "pathplanner_fixes2";
-  public static final String BUILD_DATE = "2025-02-13 19:09:40 CST";
-  public static final long BUILD_UNIX_TIME = 1739495380925L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-14 19:04:24 CST";
+  public static final long BUILD_UNIX_TIME = 1739581464464L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
