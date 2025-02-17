9a14686b62b9ca852ccbf7d7032a6cad160613d3
Hudson <arrowvark.gaming@gmail.com>
2025-02-16 23:59:37 -0600

    Added parameters to simulate.bat to allow for the automatic opening of Elastic and AdvantageScope on simulation start

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 48;
-  public static final String GIT_SHA = "5f3a02832bf6b1a79271bab059e357854df89d54";
-  public static final String GIT_DATE = "2025-02-15 15:47:24 CST";
+  public static final int GIT_REVISION = 49;
+  public static final String GIT_SHA = "32c05c024dea5b7ef3c6adb38bb152f91a777c2a";
+  public static final String GIT_DATE = "2025-02-15 17:41:31 CST";
   public static final String GIT_BRANCH = "pathplanner_fixes2";
-  public static final String BUILD_DATE = "2025-02-15 17:21:50 CST";
-  public static final long BUILD_UNIX_TIME = 1739661710598L;
+  public static final String BUILD_DATE = "2025-02-16 23:58:52 CST";
+  public static final long BUILD_UNIX_TIME = 1739771932064L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
