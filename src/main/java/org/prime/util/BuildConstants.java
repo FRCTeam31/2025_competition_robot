5294e824d531bfd2d7c793c6bd3f82a8ee488e24
ArtsNCrafters <howtoliveinpain@gmail.com>
2025-02-22 14:02:19 -0600

    improved kl path and removed comment

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 60;
-  public static final String GIT_SHA = "a4fbd62eaf7e226a6b582da5bbbaceaaa043345f";
-  public static final String GIT_DATE = "2025-02-22 10:23:26 CST";
+  public static final int GIT_REVISION = 61;
+  public static final String GIT_SHA = "8b2389c5aff353df65e53dffea789c25a4c3ed0a";
+  public static final String GIT_DATE = "2025-02-22 11:52:24 CST";
   public static final String GIT_BRANCH = "pathplanner_fixes2";
-  public static final String BUILD_DATE = "2025-02-22 11:44:25 CST";
-  public static final long BUILD_UNIX_TIME = 1740246265611L;
+  public static final String BUILD_DATE = "2025-02-22 13:38:38 CST";
+  public static final long BUILD_UNIX_TIME = 1740253118040L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
