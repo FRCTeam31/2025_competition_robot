7e00e25e9312d5e8dd5bb34bb4bb1de4012a802a
95bmwe38 <egrc0629@gmail.com>
2025-02-24 16:53:24 -0600

    Additonal merge changes

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,14 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 62;
-  public static final String GIT_SHA = "5294e824d531bfd2d7c793c6bd3f82a8ee488e24";
-  public static final String GIT_DATE = "2025-02-22 14:02:19 CST";
-  public static final String GIT_BRANCH = "pathplanner_fixes2";
-  public static final String BUILD_DATE = "2025-02-22 16:58:05 CST";
-  public static final long BUILD_UNIX_TIME = 1740265085359L;
+  public static final int GIT_REVISION = 88;
+  public static final String GIT_SHA = "7ac838a1366344a9acb911adf204c7b5089a9836";
+  public static final String GIT_DATE = "2025-02-17 19:08:10 CST";
+  public static final String GIT_BRANCH = "climber_testing";
+  public static final String BUILD_DATE = "2025-02-24 16:52:31 CST";
+  public static final long BUILD_UNIX_TIME = 1740437551961L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {
-  }
+  private BuildConstants(){}
 }
