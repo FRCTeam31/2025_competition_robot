096ec79553cf0b05b8ca4c88e742ced25266c7c3
ArtsNCrafters <howtoliveinpain@gmail.com>
2025-02-24 16:58:14 -0600

    BuildConstants being stupid and changing right after i commit for no reason

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,14 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 62;
-  public static final String GIT_SHA = "5294e824d531bfd2d7c793c6bd3f82a8ee488e24";
-  public static final String GIT_DATE = "2025-02-22 14:02:19 CST";
-  public static final String GIT_BRANCH = "pathplanner_fixes2";
-  public static final String BUILD_DATE = "2025-02-22 16:58:05 CST";
-  public static final long BUILD_UNIX_TIME = 1740265085359L;
+  public static final int GIT_REVISION = 84;
+  public static final String GIT_SHA = "35049a0d9daa2ba8e216066619bd6d82e58aba0a";
+  public static final String GIT_DATE = "2025-02-15 12:56:11 CST";
+  public static final String GIT_BRANCH = "end_effector_testing";
+  public static final String BUILD_DATE = "2025-02-24 16:55:51 CST";
+  public static final long BUILD_UNIX_TIME = 1740437751623L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {
-  }
+  private BuildConstants(){}
 }
