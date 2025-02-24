5a8dc67425546a99eb62e5e16383d09390e6f020
Zachary <zacharyipursell@gmail.com>
2025-02-24 16:58:30 -0600

    Just some build constants, also merged main into algae intake

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,20 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 97;
-  public static final String GIT_SHA = "08c6762e56b3c22819579047bace4ae589d86418";
-  public static final String GIT_DATE = "2025-02-15 12:30:20 CST";
+  public static final int GIT_REVISION = 100;
+  public static final String GIT_SHA = "cba98036b75e088bf81acb176fbf01e6bf2ba96e";
+  public static final String GIT_DATE = "2025-02-24 16:46:21 CST";
   public static final String GIT_BRANCH = "algae_intake_testing";
-  public static final String BUILD_DATE = "2025-02-15 12:32:50 CST";
-  public static final long BUILD_UNIX_TIME = 1739644370465L;
-  public static final int GIT_REVISION = 62;
-  public static final String GIT_SHA = "5294e824d531bfd2d7c793c6bd3f82a8ee488e24";
-  public static final String GIT_DATE = "2025-02-22 14:02:19 CST";
-  public static final String GIT_BRANCH = "pathplanner_fixes2";
-  public static final String BUILD_DATE = "2025-02-22 16:58:05 CST";
-  public static final long BUILD_UNIX_TIME = 1740265085359L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-24 16:56:13 CST";
+  public static final long BUILD_UNIX_TIME = 1740437773539L;
+  public static final int DIRTY = 0;
 
-  private BuildConstants() {
-  }
+  private BuildConstants(){}
 }
