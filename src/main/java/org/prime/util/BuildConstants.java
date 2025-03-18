1ffca7b93409e69d84d61304ed86f92861550791
Mason H <35619771+zeroClearAmerican@users.noreply.github.com>
2025-03-18 18:05:48 -0500

    Converted to trapezoidal and profiled PID controller for Elevator

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 218;
-  public static final String GIT_SHA = "d20090599fd24551efd11b4e4e18e734ae8d0e3b";
-  public static final String GIT_DATE = "2025-03-18 12:13:19 CDT";
-  public static final String GIT_BRANCH = "elevator_subsystem";
-  public static final String BUILD_DATE = "2025-03-18 13:32:39 CDT";
-  public static final long BUILD_UNIX_TIME = 1742322759534L;
+  public static final int GIT_REVISION = 224;
+  public static final String GIT_SHA = "75c7615172ea11fb8611784efc160d553a31c6e6";
+  public static final String GIT_DATE = "2025-03-18 15:48:38 CDT";
+  public static final String GIT_BRANCH = "elevator-profiledpid";
+  public static final String BUILD_DATE = "2025-03-18 18:03:40 CDT";
+  public static final long BUILD_UNIX_TIME = 1742339020392L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
