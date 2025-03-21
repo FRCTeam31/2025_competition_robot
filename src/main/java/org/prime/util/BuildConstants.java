185975240150355d54baa0d1d67eff8f62add2d4
James <jamesbhaney53@gmail.com>
2025-03-21 17:11:56 -0500

    moved pathplanner reef points closer to the reef. Changed kP for auto PID to 4.5

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 66;
-  public static final String GIT_SHA = "e1f9a8a3f785484843148cab298ee73dbc8bb88f";
-  public static final String GIT_DATE = "2025-03-21 16:31:27 CDT";
+  public static final int GIT_REVISION = 67;
+  public static final String GIT_SHA = "fa408ab3ee40a10a8155bf2e96fb4260553f3670";
+  public static final String GIT_DATE = "2025-03-21 16:33:05 CDT";
   public static final String GIT_BRANCH = "oktu-event-autocommit";
-  public static final String BUILD_DATE = "2025-03-21 16:31:29 CDT";
-  public static final long BUILD_UNIX_TIME = 1742592689084L;
+  public static final String BUILD_DATE = "2025-03-21 16:33:07 CDT";
+  public static final long BUILD_UNIX_TIME = 1742592787283L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
