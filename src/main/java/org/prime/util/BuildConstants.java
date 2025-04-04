2658b05595960aec6d747fd52d04afbf60508862
James <jamesbhaney53@gmail.com>
2025-04-03 22:00:52 -0500

    added a timeout to the climber

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 61;
-  public static final String GIT_SHA = "f8d4a040607512cc3de63e702bda78c2c87cae0b";
-  public static final String GIT_DATE = "2025-04-03 17:27:04 CDT";
+  public static final int GIT_REVISION = 62;
+  public static final String GIT_SHA = "8781c1532d39a9920f1a315962584b3b63d31b64";
+  public static final String GIT_DATE = "2025-04-03 17:32:25 CDT";
   public static final String GIT_BRANCH = "okok-event-autocommit";
-  public static final String BUILD_DATE = "2025-04-03 17:27:05 CDT";
-  public static final long BUILD_UNIX_TIME = 1743719225486L;
+  public static final String BUILD_DATE = "2025-04-03 17:32:26 CDT";
+  public static final long BUILD_UNIX_TIME = 1743719546640L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
